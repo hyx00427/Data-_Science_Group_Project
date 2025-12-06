02bcd066ab9972a3bd0190ee5a415749429cd376
raoza4 <raoza04@gmail.com>
2025-12-06 16:58:04 +0000

    Add files via upload

--- mean GDP global.R
+++ mean GDP global.R
@@ -20,31 +20,34 @@ youth_NEET <- read.csv("youth-not-in-education-employment-training.csv",
 left_join_GDP_world <- GDP_per_capita_world %>%
   left_join(countries_in_continents,
             join_by("Entity" == "Entity", "Code" == "Code")) %>%
-  rename_with(~ "GDP_per_capita", .cols = 4)    # rename the GDP per capita column to make it easier to work with
+  rename_with(~ "GDP_per_capita", .cols = 4) %>% # rename the GDP per capita column to make it easier to work with
+  mutate(GDP_growth = (GDP_per_capita - lag(GDP_per_capita)) * 100/ GDP_per_capita)
 
 # group the GDP values by continents and find the evolution of mean GDP for each continent
 continent_gdp_evolution <- left_join_GDP_world %>%
   filter(!is.na(Continent)) %>%    # filter N/A values from the continents column
   group_by(Continent, Year.x) %>%
-  summarise(mean_GDP = mean(GDP_per_capita, na.rm = TRUE))
+  summarise(mean_GDP_growth = mean(GDP_growth, na.rm = TRUE))
 
 # plot the evolution of mean GDP for each continent
 ggplot(continent_gdp_evolution, aes(x = Year.x,
-                                    y = mean_GDP,
+                                    y = mean_GDP_growth,
                                     color = Continent)) +
-  geom_line(linewidth = 1) +
-  scale_color_manual(values = c(
-    "Africa" = "red",
-    "Asia" = "darkgreen",
-    "Europe" = "gold",
-    "North America" = "green",
-    "Oceania" = "pink",
-    "South America" = "purple")) +
+  geom_line(linewidth = 0.5) +
+  scale_color_manual(values = c("Africa" = "red",
+                                "Asia" = "darkgreen",
+                                "Europe" = "gold",
+                                "North America" = "green",
+                                "Oceania" = "pink",
+                                "South America" = "purple")) +
   labs(x = "Year",
-       y = "Average GDP per capita",
-       title = "Evolution of average GDP per capita  for the continents",
+       y = "Percentage growth of GDP per capita (%)",
+       title = "Average growth of GDP per capita globally",
        color = "Continent") +
+  scale_x_continuous(limits = c(1991,2021)) +
+  scale_y_continuous(limits = c(-50, 25)) +
   theme_minimal() +
-  theme(plot.title = element_text(hjust = 0.5))
-  
+  theme(plot.title = element_text(hjust = 0.5,
+                                  size = 14,
+                                  face = "bold"))
 
